4fc670f4753258f5f4c8e110c1b0de540012b02e
Dami Lee <50016477+iamdami@users.noreply.github.com>
2022-03-18 16:51:32 +0900

    Update readGray1.py

--- 22-03-17/readGray1.py
+++ 22-03-17/readGray1.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture("night.mp4")
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame", gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
