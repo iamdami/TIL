892ebbf08b2e759f353f5f971ccc5dedfc69eb85
Dami Lee <onemoredami@gmail.com>
2022-06-22 10:40:04 +0900

    Update cocoJsonParsing.py

--- 22-06-13/cocoJsonParsing.py
+++ 22-06-13/cocoJsonParsing.py
@@ -1,46 +1,52 @@
-import json,cv2
-jsonPath = "annotations/instances_train2017.json"
-with open(jsonPath,"r") as f:
-    fileReadInstance = f.read()
-valjson = json.loads(fileReadInstance)
-print(len(valjson['annotations']))
-for valAnno in valjson['annotations']:
-    # image의 box좌표정보
-    boxPT = valAnno['bbox']
-    
-    ######################
-    float_xCenter,float_yCenter,float_width,float_height = valAnno['bbox']
-    minX = int(float_xCenter)
-    minY = int(float_yCenter)
-    width = int(float_width)
-    height = int(float_height)
+import json, cv2
+# import time
 
+jsonPath = "../cocoToYoloFormat/annotations/instances_train2017.json"
+
+with open(jsonPath, 'r') as f:
+    fReadInstance = f.read()
+
+loadJson = json.loads(fReadInstance)
+# print(len(loadJson['annotations']))
+
+for anno in loadJson['annotations']:
+    bboxPt = anno['bbox']
+
+    float_minX, float_minY, float_width, float_height = anno['bbox']
+    minX = int(float_minX)
+    minY = int(float_minY)
+    width= int(float_width)
+    height = int(float_height)
     # print(minX, minY, width, height)
 
-    maxX = minX + width
-    maxY = minY + height
-
+    maxX = minX + width    
+    maxY = minY + height    
     # print(maxX, maxY)
-    imgName = f"{str(valAnno['image_id']).zfill(12)}.jpg"
-    img = cv2.imread(f"train2017/{imgName}")
-    h,w,c = img.shape
-    print(h,w)
-    x1,y1,cw,ch = ((minX+maxX)/2)/w,((minY+maxY)/2)/h,(abs(minX-maxX))/w,(abs(minY-maxY))/h
-    print(x1,y1,cw,ch)
-    boxPT = [x1,y1,cw,ch]
-    # recImg = cv2.rectangle(img, (minX, minY), (maxX, maxY), (255, 0, 0), 3)
-    # cv2.imwrite("./recImg.jpg", recImg)
-    # break
-    ######################
 
-    # 레이블 정보
-    categoryID = valAnno['category_id']
-    ptLabel = ' '.join(list(map(str,boxPT)))
-    # txt파일로 write 할 내용
-    writeText = f"{categoryID} {ptLabel}"
-    textFlieName = f"{str(valAnno['image_id']).zfill(12)}.txt"
+    imgName = f"{str(anno['image_id']).zfill(12)}.jpg"
+    readImg = cv2.imread(f"train2017/{imgName}")
+    h, w, c = readImg.shape
+    # print(h, w)
 
-    print(f"txtFileName : {textFlieName}")
-    print(f"txtFileText : {writeText}")
-    with open(f"train_label/{textFlieName}",'a') as f:
-        f.write(f"{writeText}\n")
+    centerX = ((minX+maxX)/2)/w 
+    centerY = ((minY+maxY)/2)/h
+    centerW = (abs(minX-maxX)/2)/w
+    centerH = (abs(minY+maxY)/2)/h
+    # print(centerX, centerY, centerW, centerH)
+
+    bboxPt = [centerX, centerY, centerW, centerH]
+
+    txtFileName = f"{str(anno['image_id']).zfill(12)}.txt"
+
+    catID = anno['category_id']
+    labelPt = " ".join(list(map(str, bboxPt)))
+
+    writeTxt = f"{catID} {labelPt}"
+
+    print(f"text File Name: {txtFileName}")
+    print(f"text: {writeTxt}")
+
+    with open(f"trainLabel/{txtFileName}", 'a') as f:
+        f.write(f"{writeTxt}\n")
+
+    # time.sleep(20)
