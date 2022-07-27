9b1912df03d141408363a4f9dc879c46ece75c36
Dami Lee <onemoredami@gmail.com>
2022-07-27 16:11:30 +0900

    Update and rename cocoToYolo.py to YoloRecImg.py

--- 22-06-13/YoloRecImg.py
+++ 22-06-13/YoloRecImg.py
@@ -1,11 +1,12 @@
 import cv2
 import imagesize
 
-imgPath = "../../cocoToYoloFormat/train/images/000000168622_jpg.rf.f114faaa06c2b44134f479a97ca014ba.jpg"
+imgPath = ""
 img = cv2.imread(imgPath)
 
 w, h = imagesize.get(str(imgPath))
 
+# yolo bbox value
 xCenter = 0.4921875
 yCenter = 0.5958333333333333
 width = 0.303125
@@ -21,12 +22,8 @@ minY = int(float_yCenter - (float_height / 2))
 width = int(float_width)
 height = int(float_height)
 
-# print(minX, minY, width, height)
-
 minX2 = minX + width
 minY2 = minY + height
-
-# print(minX2, minY2)
 
 recImg = cv2.rectangle(img, (minX, minY), (minX2, minY2), (255, 0, 0), 3)
 cv2.imwrite("./recImg.jpg", recImg)
