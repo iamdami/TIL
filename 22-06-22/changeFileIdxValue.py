2dfabdfd7fdce6f4e352e503e9c0621be9113174
Dami Lee <onemoredami@gmail.com>
2022-07-29 11:06:43 +0900

    Update changeFileIdxValue.py

--- 22-06-22/changeFileIdxValue.py
+++ 22-06-22/changeFileIdxValue.py
@@ -8,18 +8,17 @@ dirPath = "labels/val/"
 fileName = "*.txt"
 inputNum = input("your file Num : ")
 print(inputNum.isdigit())
-lableFileList = glob.glob(dirPath + fileName)
-print(len(lableFileList))
-if inputNum.isdigit() and len(lableFileList) == int(inputNum):
-    for lableFile in lableFileList:
-        # print(lableFile)
-        with open(lableFile, "r") as f:
+labelFileList = glob.glob(dirPath + fileName)
+print(len(labelFileList))
+if inputNum.isdigit() and len(labelFileList) == int(inputNum):
+    for labelFile in labelFileList:
+        with open(labelFile, "r") as f:
             data = f.read()
         data = data.replace("3 ", "2 ")
         print(data)
 
-        with open(lableFile, "w") as f:
+        with open(labelFile, "w") as f:
             f.write(data)
 else:
-    print(f"your file Num : {len(lableFileList)}")
+    print(f"your file Num : {len(labelFileList)}")
     print("or your input type not int")
