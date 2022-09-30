e928f1643f883fdb75a4ff03582cdf8c29b40830
Dami <onemoredami@gmail.com>
2022-09-30 15:22:08 +0900

    Update face_half_preprocessing.py

--- python/face_half_preprocessing.py
+++ python/face_half_preprocessing.py
@@ -1,6 +1,11 @@
+# 얼굴 크롭이미지 얼굴 윗부분으로만 절반 잘라서 지정 폴더에 저장
+
 import os, cv2
 
-dataRoot = "../face/augmentation/"
+dataRoot = "../facekcl/augmentation/"
+dataRootHalf = "../facekcl/augmentation_half/"
+if not os.path.isdir(dataRootHalf):
+    os.mkdir(dataRootHalf)
 personIdxList = os.listdir(dataRoot)
 
 for personIdx in personIdxList:
@@ -13,6 +18,8 @@ for personIdx in personIdxList:
         h, w, c = imgRead.shape
         half_h = int(h / 2)
         cropImg = imgRead[:half_h, :w]
-        cv2.imwrite("asd.jpg", cropImg)
-        break
-    break
+        
+        dataRootIdxHalf = os.path.join(dataRootHalf,personIdx)
+        if not os.path.isdir(dataRootIdxHalf):
+            os.mkdir(dataRootIdxHalf)
+        cv2.imwrite(os.path.join(dataRootIdxHalf,imgName), cropImg)
