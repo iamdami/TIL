dd3e37dce08378204595bf926b9ffece9329a49b
Dami <onemoredami@gmail.com>
2023-04-09 22:52:42 +0900

    Update checkOverlaps.py

--- python/checkOverlaps.py
+++ python/checkOverlaps.py
@@ -1,63 +1,33 @@
-# from os import walk
-
-# imgPath = "./val2017"
-# labelpath = "./label"
-
-# imgRes = []
-# labelRes = []
-
-# for (dirPath, dirName, fileName) in walk(imgPath):
-#     imgRes.extend(fileName)
-#     rmv_imgRes = ".jpg"
-#     imgRes = [elem.replace(rmv_imgRes, "") for elem in imgRes]
-
-# for (dirPath, dirName, fileName) in walk(labelpath):
-#     labelRes.extend(fileName)
-#     rmv_labelRes = ".txt"
-#     labelRes = [elem.replace(rmv_labelRes, "") for elem in labelRes]
-
-# # print(imgRes)
-# # print(labelRes)
-
-# if (imgRes != labelRes):
-#     notInImgRes = [elem for elem in imgRes if elem not in labelRes]
-#     notInLabelRes = [elem for elem in labelRes if elem not in imgRes]
-#     print(len(notInImgRes))
-#     print(notInLabelRes)
-    
-# else:
-#     pass
-
-
-
-
 import os
-imgPath = "./train2017"
-labelpath = "./train_label"
+imgPath = "Pytorch_Retinaface/faceImg/SB"
+labelpath = "Pytorch_Retinaface/faceLabelCroped/labelCroped_SB"
 imgList = os.listdir(imgPath)
 labelList = os.listdir(labelpath)
 print(len(imgList))
 print(len(labelList))
 
-delNameList = list()
+delNameList = []
 if len(labelList) < len(imgList):
     for imgName in imgList:
         delNameList.append(imgName.split('.')[0])
     for labelName in labelList:
-        delNameList.remove(labelName.split('.')[0])
+        if labelName.split('.')[0] in delNameList:
+            delNameList.remove(labelName.split('.')[0])
     print(f"1: {delNameList}")
 elif len(labelList) > len(imgList):
     for labelName in labelList:
-        delNameList.remove(labelName.split('.')[0])
-    print(f"2: {delNameList}")
+        delNameList.append(labelName.split('.')[0])
     for imgName in imgList:
-        delNameList.append(imgName.split('.')[0])
+        if imgName.split('.')[0] in delNameList:
+            delNameList.remove(imgName.split('.')[0])
+    print(f"2: {delNameList}")
 else:
     print("Equal")
     exit()
-for i,delName in enumerate(delNameList):
+    
+for delName in delNameList:
     delName = f"{delName}.jpg"
     os.system(f"rm -rf {os.path.join(imgPath,delName)}")
 
-print(len(imgList))
-print(len(labelList))
+print(f"after: {len(os.listdir(imgPath))}")
+print(f"after: {len(os.listdir(labelpath))}")
